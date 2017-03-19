e7a4d6c0957d6b22b3626bf28b468e7e628f69e3
Helena Cruz <helcruz8@gmail.com>
2017-03-19 16:01:01 +0000

    added test files and a script to run them

---
+++ tests/run_tests.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+import time
+
+# colors
+
+RED   = "\033[1;31m"  
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+
+# commands
+
+serial_command = (['g++', '../life3d.cpp', '-o', 'life3d', '-std=c++11'])
+openmp_command = (['g++', '-fopenmp', '../life3d.cpp', '-o', 'life3d', '-std=c++11'])
+
+# get files
+
+dir_files = []
+for file in os.listdir('.'):
+    if file.endswith('.out'):
+       dir_files += [file]
+
+files = []
+
+for file in dir_files:
+    files += [str.split(file, '.')]
+
+# run serial
+
+subprocess.run(serial_command)
+
+for file in files:
+    name = file[0] + '.' + file[1]
+    with open(name + '.myout') as outfile:
+        run_command = (['./life3d', file[0] + '.in', file[1]]);
+        print(BOLD + ' '.join(run_command) + RESET)
+        start = time.time() 
+        subprocess.run(run_command, stdout=outfile)
+        end = time.time()
+        outfile.close()
+        diff_command = (['diff', name + '.out', name + '.myout'])
+        result = subprocess.run(diff_command, stdout=subprocess.PIPE)
+        print(RESET + "Time passed: %.4f" % (end - start) + "s")
+        if result.stdout.decode() == '':
+            print(GREEN + "Test successful" + RESET)
+        else:
+            print(RED + "Test failed" + RESET)
+
+# run openmp
+
+# run mpi
+
